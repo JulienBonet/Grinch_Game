0361ed7d5a6706519599ae2f6ad1e287c70aba67
JULIEN BONET <julienbonetpro@gmail.com>
2024-02-21 15:59:34 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import PageFinal from "./components/Page_final/PageFinal.jsx";
+import Menu from "./components/Menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/finale",
+        element: <PageFinal />,
+      },
+      { path: "/menu", element: <Menu /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
